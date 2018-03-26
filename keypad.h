05ee72c0f62f6ebe3d83ce97831737fbfe0377fc
Fahad Siddiqui <faahadms@gmail.com>
2018-03-26 11:37:56 +0530

    Add files via upload

---
+++ keypad.h
@@ -0,0 +1,22 @@
+#ifndef __KEYPAD_H__
+	#define __KEYPAD_H__
+	
+	
+	typedef enum {
+		
+	Numeric = 0,
+	Alpha
+	}Keymode;
+	
+	void CreateKeypad(WM_HWIN hWin);
+	typedef struct {
+		
+		uint16_t Current_Editbox;
+		uint16_t Max_Len;
+		Keymode Start_Mode;
+	}keyboard_Data_t;
+	
+	void Config_Keyboard(uint16_t Max_Len, uint32_t Editbox_Id, Keymode Mode);
+#endif
+
+/******************/
